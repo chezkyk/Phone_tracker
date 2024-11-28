1b07b3f5173e94b39d10e0a65121fc9e75fe1ac5
chezkyk <Y0548532123@GMAIL.COM>
2024-11-28 15:30:32 +0200

    added get info on direct connection between two devices rout.

--- app/routs/phone_blueprint.py
+++ app/routs/phone_blueprint.py
@@ -30,15 +30,14 @@ def get_interaction():
 
 @phone_bp.route("/api/get-all-devices-connecting-by-bluetooth", methods=['GET'])
 def get_all_devices_connecting_by_bluetooth():
-    max_depth = int(request.args.get('max_depth', 4))
     tracker = PhoneTracker(neo4j_driver)
-    connections = tracker.get_all_bluetooth_connections(max_depth)
+    connections = tracker.get_all_bluetooth_connections()
     return jsonify({"connections": connections}), 200
 
 @phone_bp.route("/get-all-devices-with-strength-stronger-than_60", methods=['GET'])
 def get_all_devices_with_strength_stronger_than_60():
     tracker = PhoneTracker(neo4j_driver)
-    connections = tracker.get_all_bluetooth_connections()
+    connections = tracker.get_all_devices_strength_stronger_than_60()
     return jsonify({"connections": connections}), 200
 
 @phone_bp.route("/get-count-of-all-devices-connected-to-device-by-id/<device_id>", methods=['GET'])
@@ -47,11 +46,24 @@ def get_count_of_all_devices_connected_to_device_by_id(device_id):
     count = tracker.count_device_connections(device_id)
     return jsonify({"device_id": device_id, "connection_count": count}), 200
 
-# @phone_bp.route("/", methods=['GET'])
-# def get_info_on_direct_connection_between_two_devices():
-#    pass
-#
-# @phone_bp.route("/", methods=['GET'])
-# def get_recent_info_for_device_sorted_by_timestamp():
-#    pass
+@phone_bp.route("/get-info-on-direct-connection-between-two-devices", methods=['GET'])
+def get_info_on_direct_connection_between_two_devices():
+    device_id1 = request.args.get("device_id1")
+    device_id2 = request.args.get("device_id2")
+    if not device_id1 or not device_id2:
+        return jsonify({"error": "Both device IDs are required"}), 400
+    tracker = PhoneTracker(neo4j_driver)
+    as_direct_connection = tracker.get_info_on_direct_connection(device_id1,device_id2)
+    return jsonify({
+        "device1": device_id1,
+        "device2": device_id2,
+        "is_connected": as_direct_connection
+    }), 200
+
+
+@phone_bp.route("/get-recent-info-for-device-sorted-by-timestamp", methods=['GET'])
+def get_recent_info_for_device_sorted_by_timestamp():
+    tracker = PhoneTracker(neo4j_driver)
+    connections = tracker.get_recent_info_sorted_by_timestamp()
+    return jsonify({"connections": connections}), 200
 
